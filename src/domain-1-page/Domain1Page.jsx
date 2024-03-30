1dfccde3e576acfbc4ed2f1ecba722687e73ee0b
David Joy <davjoy@gmail.com>
2024-03-30 08:47:31 -0400

    fix: fixing a testing typo

--- src/domain-1-page/Domain1Page.jsx
+++ src/domain-1-page/Domain1Page.jsx
@@ -3,7 +3,7 @@ import './index.scss';
 
 const Domain1Page = () => (
   <Container>
-    <h2>Domain Ones</h2>
+    <h2>Domain One</h2>
     <p>
       I am loaded as a remote script, and I am getting react, react-dom, and paragon from the
       shell application.  I also specify my own stylesheet that turns h1 tags to green.
